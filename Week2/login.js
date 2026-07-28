e88c252fa6892a831fd8809cccdd6682a8feb610
byeh117 <byehhalo@gmail.com>
2026-07-28 14:01:41 -0400

    7-28-2026

---
+++ Week2/login.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#loginForm");
+const message = document.querySelector("#message");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+
+    if(username === "admin" && password === "password123"){
+        message.computedStyleMap.color = "green";
+        message.textContent = "Login Successful!";
+
+        // redirect to dashboard page:
+        // window.location.href = "dashboard.html"
+    } else {
+        message.computedStyleMap.color = "red";
+        message.textContent = "Invalid username or password";
+    }
+});
---
+++ Week2/week2day1.js
@@ -0,0 +1,20 @@
+// function add(a, b) {
+//     return a + b;
+// }
+
+// const add = (a, b) => a + b;
+
+// function timer() {
+//     this.seconds = 0;
+
+//     setInterval(() => {
+//         this.seconds++;
+//         console.log(this.seconds);
+//     }, 1000);
+// }
+
+let num = 1599.1547;
+
+let result = num.toFixed(2);
+
+console.log(result);
